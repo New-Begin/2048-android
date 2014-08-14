f36f4247656faee0933a145660bbb754d01cdcf5
yysnow <978510569@qq.com>
2014-08-14 20:20:24 +0800

    test1

--- src/com/newbegin/android_2048/MainActivity.java
+++ src/com/newbegin/android_2048/MainActivity.java
@@ -6,7 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	public int hello;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
