e524dd3c785bb387f0cf79b0781320e8f1266e39
YingQi Hu <mr.hoo7793@gmail.com>
2014-08-14 20:29:47 +0800

    test

--- src/com/newbegin/android_2048/MainActivity.java
+++ src/com/newbegin/android_2048/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 public class MainActivity extends Activity {
 	public int hello;
 	
+	public int b;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
