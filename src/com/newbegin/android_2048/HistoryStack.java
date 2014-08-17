c9ff5257e3f3f1ec8be508fbfb1a253e284a220a
YingQi Hu <mr.hoo7793@gmail.com>
2014-08-17 21:07:12 +0800

    HistoryStack

---
+++ src/com/newbegin/android_2048/HistoryStack.java
@@ -0,0 +1,13 @@
+package com.newbegin.android_2048;
+
+import java.util.Stack;
+
+public class HistoryStack extends Stack<Card[][]> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 961640924660228196L;
+	
+
+}
